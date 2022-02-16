5155638f150b8352374ff4c2ea3ce8c3b90e5c20
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-16 19:35:49 +0300

    was added styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
+
 
 export default function FirstPost() {
-    return <>
-        <Head><title>First Post title</title></Head>
+    return <Layout>
+        <Head>
+            <title>First Post title</title>
+        </Head>
         <h1>First post</h1>
         <h2><Link href={'/'}> Back Home </Link></h2>
-    </>
+    </Layout>
 }
