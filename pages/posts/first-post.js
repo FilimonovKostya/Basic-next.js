bd270dfaa67609fc846771f17f8ab2ab451da0d6
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-16 19:22:15 +0300

    changed title of page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,9 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function FirstPost() {
     return <>
+        <Head><title>First Post title</title></Head>
         <h1>First post</h1>
         <h2><Link href={'/'}> Back Home </Link></h2>
     </>
