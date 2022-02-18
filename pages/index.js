688c304cbc201eb0b731844457d904d56757b326
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-18 12:07:04 +0300

    added links

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Layout from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/post'
-
+import {getSortedPostsData} from '../lib/post'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData()
@@ -15,22 +15,24 @@ export async function getStaticProps() {
 
 export default function Home({allPostsData}) {
 
-  return (
-      <Layout home>
-          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-              <h2 className={utilStyles.headingLg}>Blog</h2>
-              <ul className={utilStyles.list}>
-                  {allPostsData.map(({ id, date, title }) => (
-                      <li className={utilStyles.listItem} key={id}>
-                          {title}
-                          <br />
-                          {id}
-                          <br />
-                          {date}
-                      </li>
-                  ))}
-              </ul>
-          </section>
-      </Layout>
-  )
+    return (
+        <Layout home>
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({id, date, title}) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br/>
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date}/>
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
